d8873d45972f5c98ed6c3ec0bfa6f4f52d403d4f
KrzysiekBlankiewicz <krzysiek.wiggin.blankiewicz@gmail.com>
2024-05-14 15:06:10 +0200

    list task - first approach

--- praktyka/zad1.cpp
+++ praktyka/zad1.cpp
@@ -1,8 +1,33 @@
 
 #include <iostream>
 #include <list>
+#include <string>
+
+struct osoba{
+    std::string first_name;
+    std::string last_name;
+    int age;
+};
+
+void print_list(std::list<osoba>* list_address)
+{
+    for(auto i : *list_address)
+        std::cout << i.first_name << " " << i.last_name << " " << i.age << std::endl;
+}
 
 int main()
 {
-    std::cout << "hello" << std::endl;
+    std::list<osoba> list;
+
+    osoba x;
+    std::cin >> x.first_name >> x.last_name >> x.age;
+
+    osoba y;
+    std::cin >> y.first_name >> y.last_name >> y.age;
+    
+    list.push_back(x);
+    list.push_back(y);
+    
+    //std::cout << list.front().age << std::endl;
+    print_list(&list);
 }
