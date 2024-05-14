88c12cf2a66d78b1166173a5c1a5c62458901a99
KrzysiekBlankiewicz <krzysiek.wiggin.blankiewicz@gmail.com>
2024-05-14 15:36:52 +0200

    filling list, printing & removing second elem

--- praktyka/zad1.cpp
+++ praktyka/zad1.cpp
@@ -3,6 +3,8 @@
 #include <list>
 #include <string>
 
+const int n = 2;
+
 struct osoba{
     std::string first_name;
     std::string last_name;
@@ -11,23 +13,44 @@ struct osoba{
 
 void print_list(std::list<osoba>* list_address)
 {
-    for(auto i : *list_address)
-        std::cout << i.first_name << " " << i.last_name << " " << i.age << std::endl;
+    if(list_address != nullptr)
+        for(auto i : *list_address)
+            std::cout << i.first_name << " " << i.last_name << " " << i.age << std::endl;
+}
+
+osoba osoba_from_cin()
+{
+    std::string f_name, l_name;
+    int age;
+
+    std::cin >> f_name >> l_name >> age;
+    
+    return osoba{f_name, l_name, age};
+}
+
+void remove_second_position(std::list<osoba>* list_address)
+{
+    if(list_address->size() >= 2)
+    {
+        osoba first_elem = list_address->front();
+        list_address->pop_front();
+        list_address->pop_front();
+        list_address->push_front(first_elem);
+    }
 }
 
 int main()
 {
     std::list<osoba> list;
 
-    osoba x;
-    std::cin >> x.first_name >> x.last_name >> x.age;
+    int input_size = n;
 
-    osoba y;
-    std::cin >> y.first_name >> y.last_name >> y.age;
-    
-    list.push_back(x);
-    list.push_back(y);
-    
-    //std::cout << list.front().age << std::endl;
+    for(int i = 0; i < input_size; ++i){
+        list.push_back(osoba_from_cin());
+    }
     print_list(&list);
+    
+    remove_second_position(&list);
+    print_list(&list);
+
 }
