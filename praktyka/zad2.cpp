88748ad23ee1a633e81f6da7df64983db0ee34a1
KrzysiekBlankiewicz <krzysiek.wiggin.blankiewicz@gmail.com>
2024-05-14 16:37:44 +0200

    zad2 działa - bezpiecznie, ale nieefektywnie

--- praktyka/zad2.cpp
+++ praktyka/zad2.cpp
@@ -1,20 +1,29 @@
-#include<iostream>
+#include <iostream>
+#include <limits.h>
+#include <math.h>
 
-unsigned long long factorial(int number)
-{
-    if(number < 0)
-        return 0;
-    else
-        return factorial_inside(number);    
-}
 
 unsigned long long factorial_inside(int number)
 {
     if(number == 0)
         return 1;
 
-    unsigned long long result = factorial(number-1)*number;
-    return result;
+    unsigned long long previous = factorial_inside(number-1);
+
+    if(previous >= (ULLONG_MAX/number))
+        return 0;
+    else
+        return previous*number;
+}
+
+unsigned long long factorial(int number)
+{
+    if(number < 0)
+        return 0;
+    else
+    {
+        return factorial_inside(number);
+    }
 }
 
 int main()
@@ -22,5 +31,8 @@ int main()
     int x;
     std::cin >> x;
     unsigned long long result = factorial(x);
-    std::cout << x << "! = " << result << std::endl;
+    if(result == 0)
+        std::cout << "error" << std::endl;           
+    else 
+        std::cout << x << "! = " << result << std::endl;
 }
